f51fed0c68ff47e25ab0a446f0cf10f344db0b8f
Steve Louie <stephen.r.louie@gmail.com>
2018-01-06 16:40:09 -0500

    updating to draft 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,44 @@
 import React from 'react'
 import Link from 'gatsby-link'
-export default () =>
-  <div style={{ color: `tomato` }}>
-    <h1>Hello World!</h1>
-    <p>This personal site is under construction</p>
-    <img src="https://upload.wikimedia.org/wikipedia/en/c/c5/Bob_the_builder.jpg" alt="" />
-    <Link to="/page-2/">Go to page 2</Link>
-    <div>
-      <Link to="/counter/">Counter</Link>
-    </div>
-  </div>;
+import Container from "../components/container";
+import styles from "./index.module.css";
 
-// const IndexPage = () => (
-//   <div>
-//     <h1>This is a test</h1>
-//     <p>Welcome to your new Gatsby site.</p>
-//     <p>Now go build something great.</p>
-//     <Link to="/page-2/">Go to page 2</Link>
-//   </div>
-// )
-//
-// export default IndexPage
+const User = props =>
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.section}>Profession</h2>
+      <p className={styles.excerpt}>
+        {props.professional}
+      </p>
+      <h2 className={styles.section}>Academic</h2>
+      <p className={styles.excerpt}>
+        {props.academic}
+      </p>
+      <h2 className={styles.section}>Miscellaneous</h2>
+      <p className={styles.excerpt}>
+        {props.misc}
+      </p>
+    </div>
+  </div>
+
+export default () =>
+  <div>
+    <Container>
+      <User
+        avatar="https://github.com/stephenrlouie/site/raw/draft1/img/headshot.jpg"
+        professional=
+        "I'm currently a Software Engineer at Cisco Systems working
+        out of the Greater Boston area. I primarily develop in Go
+        and focus on writing microservices to be deployed on Kubernetes."
+        academic=
+        "I graduated from Lehigh University with a Bachelors in Integrated
+        Business and Engineering in 2014. I also received my Masters in
+        Computer Science from Lehigh University in 2015."
+        misc=
+        "You can find me Lindy Hopping at least once a week in the Boston
+        area. I enjoy cooking, playing board games and video games, and
+        being active in anyway (sports, rowing, lifting)."
+      />
+    </Container>
+  </div>;
