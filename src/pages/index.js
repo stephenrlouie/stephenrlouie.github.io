989ba1d6075dfcb5715a10d18450d3e73d561b35
Steve Louie <stephen.r.louie@gmail.com>
2018-01-07 12:13:13 -0500

    refactored the elements

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,39 +3,38 @@ import Link from 'gatsby-link'
 import Container from "../components/container";
 import styles from "./index.module.css";
 
-const User = props =>
-  <div className={styles.user}>
-    <img src={props.avatar} className={styles.avatar} alt="" />
-    <div className={styles.description}>
-      <h2 className={styles.section}>Profession</h2>
-      <p className={styles.excerpt}>
-        {props.professional}
-      </p>
-      <h2 className={styles.section}>Academic</h2>
-      <p className={styles.excerpt}>
-        {props.academic}
-      </p>
-      <h2 className={styles.section}>Miscellaneous</h2>
-      <p className={styles.excerpt}>
-        {props.misc}
-      </p>
-    </div>
+const Section = props =>
+  <div>
+    <h2>
+      {props.title}
+    </h2>
+    <p>
+      {props.text}
+    </p>
   </div>
-  
+
 export default () =>
   <div>
+    <img src="https://github.com/stephenrlouie/site/raw/master/img/headshot.jpg" className={styles.avatar} alt="Steve's Picture" />
     <Container>
-      <User
-        avatar="https://github.com/stephenrlouie/site/raw/master/img/headshot.jpg"
-        professional=
+      <Section
+        title="Profession"
+        text=
         "I'm currently a Software Engineer at Cisco Systems working
-        out of the Greater Boston area. I primarily develop in Go
-        and focus on writing microservices to be deployed on Kubernetes."
-        academic=
+        out of the Greater Boston area. Most of my experience has been writing,
+        distributed systems, and orchestration software. I mostly write Go
+        to develop microservices to be deployed on Kubernetes."
+      />
+      <Section
+        title="Academic"
+        text=
         "I graduated from Lehigh University with a Bachelors in Integrated
         Business and Engineering in 2014. I also received my Masters in
         Computer Science from Lehigh University in 2015."
-        misc=
+      />
+      <Section
+        title="Miscellaneous"
+        text=
         "You can find me Lindy Hopping at least once a week in the Boston
         area. I enjoy cooking, playing board games and video games, and
         being active in anyway (sports, rowing, lifting)."
